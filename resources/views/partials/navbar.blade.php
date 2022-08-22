b6f8acaafee1e6e49904c1e49a10693b18d0b074
RaksaMenolakSadar <rakhshamuhammad@gmail.com>
2022-08-22 15:42:40 +0700

    commit kelima, menghilangkan toggler

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,8 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container">
         <a class="navbar-brand" href="/">Catatan Perjalanan</a>
-        <button class="navbar-toggler position-absolute mt-auto collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
